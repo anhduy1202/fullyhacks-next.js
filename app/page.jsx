638ab4b3d8d6b965869ed98031573748534a484d
Daniel Truong <aduy1122@gmail.com>
2023-04-07 16:20:40 -0700

    removed fetch

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,6 @@
 import Link from "next/link";
-import { prisma } from "../db";
-import Form from "./components/Form/Form";
-
-const fetchAllNotes = async () => {
-  const notes = await prisma.Note.findMany();
-  return notes;
-};
 
 export default async function Home() {
-  const data = await fetchAllNotes();
-  console.log(data);
   return (
     <section className="flex flex-col gap-8">
       <h1 className="text-[4rem] font-bold"> FullyNotes </h1>
