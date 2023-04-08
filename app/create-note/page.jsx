ba1eeaac76e3c32fc37e775ac3f148806843ccb8
Daniel Truong <aduy1122@gmail.com>
2023-04-08 01:10:40 -0700

    setup done

--- app/create-note/page.jsx
+++ app/create-note/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CreateNotePage = () => {
   return (
     <div className="">
-      <p className="text-[4rem] font-bold"> Create my note </p>
+      <p className="text-[4rem] font-bold">Make A Note</p>
     </div>
   );
 };
